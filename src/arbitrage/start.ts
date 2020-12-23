7efc456a677f8e0f36dc8b99a26ea0d57380a4a1
bakuchi- <helveticaf0015se@gmail.com>
2020-12-23 17:02:27 +0900

    arb startを修正

--- src/arbitrage/start.ts
+++ src/arbitrage/start.ts
@@ -2,12 +2,15 @@ import { recordXRPArb } from './xrp-arb';
 import { recordFiatArb } from './fiat-crypto-arb';
 import { repeat } from '../utils/repeat';
 
-const TIMEOUT = Number(process.env.TIMEOUT) || 3600 * 1000
-const INTERVAL = Number(process.env.INTERVAL) || 180
+const TIMEOUT = Number(process.env.TIMEOUT) || 240 * 1000
+const INTERVAL = Number(process.env.INTERVAL) || 120
 const expiration = Date.now() + TIMEOUT;
+
 (async () => {
     repeat(recordXRPArb, INTERVAL, expiration);
     repeat(recordFiatArb, INTERVAL, expiration);
+    // recordXRPArb();
+    // recordFiatArb();
 })().catch(e => {
     console.log('e :>> ', e);
     process.exit(1);
