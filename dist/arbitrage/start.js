0effb38f3a750741532c135a1425a152c486c348
bakuchi- <helveticaf0015se@gmail.com>
2020-12-24 16:28:20 +0900

    fix

--- dist/arbitrage/start.js
+++ dist/arbitrage/start.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const xrp_arb_1 = require("./xrp-arb");
 const fiat_crypto_arb_1 = require("./fiat-crypto-arb");
 const config_1 = __importDefault(require("../config/config"));
@@ -11,6 +12,7 @@ const INTERVAL = Number(process.env.INTERVAL) || 120;
 const expiration = Date.now() + TIMEOUT;
 const column = ['BTC', 'XRP'];
 const xrpColumn = ['BTC', 'ETH'];
+const app = express_1.default();
 const main = async () => {
     try {
         if (expiration * 1000 < Date.now())
@@ -25,9 +27,22 @@ const main = async () => {
         console.log('[Info]:EXIT(1)');
         process.exit(1);
     }
-    setTimeout(async () => await main(), INTERVAL * 1000);
+    // setTimeout(async () => await main(), INTERVAL * 1000);
 };
-main().catch(e => {
-    console.log('e :>> ', e);
-    process.exit(1);
+app.get('/cron/start/bot', (req, res) => {
+    console.log('req.headers :>> ', req.headers);
+    console.log('req.ip :>> ', req.ip);
+    if (req.headers['X-Appengine-Cron']) {
+        console.log('This reqest is from GAE');
+        setImmediate(async () => await main());
+        res.status(200).end();
+    }
+    res.send("Hello World!");
 });
+console.log('[Info]:App started...');
+const port = process.env.port || 3000;
+app.listen(port, () => console.log('🚀App listening on port', port));
+// main().catch(e => {
+//     console.log('e :>> ', e);
+//     process.exit(1);
+// })
