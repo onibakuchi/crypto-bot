44e224de605e76dd7f89c9175d77f63951da6183
bakuchi- <helveticaf0015se@gmail.com>
2020-12-24 16:21:35 +0900

    fix

--- src/arbitrage/start.ts
+++ src/arbitrage/start.ts
@@ -1,13 +1,17 @@
+import express from 'express';
 import { recordXRPArb } from './xrp-arb';
 import { recordFiatArb } from './fiat-crypto-arb';
 import CONFIG from '../config/config';
 
 const TIMEOUT = Number(process.env.TIMEOUT) || 480 * 1000
 const INTERVAL = Number(process.env.INTERVAL) || 120
+
 const expiration = Date.now() + TIMEOUT;
 
 const column = ['BTC', 'XRP'];
 const xrpColumn = ['BTC', 'ETH'];
+
+const app = express();
 
 const main = async () => {
     try {
@@ -24,7 +28,24 @@ const main = async () => {
     // setTimeout(async () => await main(), INTERVAL * 1000);
 }
 
-main().catch(e => {
-    console.log('e :>> ', e);
-    process.exit(1);
-})
+app.get('/cron/start/bot', (req, res) => {
+    console.log('req.headers :>> ', req.headers);
+    console.log('req.ip :>> ', req.ip);
+
+    if (req.headers['X-Appengine-Cron']) {
+        console.log('This reqest is from GAE');
+        setImmediate(async () => await main());
+        res.status(200).end();
+    }
+    res.send("Hello World!");
+});
+
+console.log('[Info]:App started...');
+const port = process.env.port || 3000;
+app.listen(port, () => console.log('🚀App listening on port', port));
+
+
+// main().catch(e => {
+//     console.log('e :>> ', e);
+//     process.exit(1);
+// })
